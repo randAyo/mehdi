1cf7d9066e60d757c4d36f7b69a54819afce1d11
yel <aot14121@gmail.com>
2021-02-24 21:51:32 +0100

    ?

---
+++ pages/posts/[id].js
@@ -0,0 +1,40 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+  const lang = postData.language === "ar" ? "rtl" : "ltr";
+  return (
+    <Layout>
+      <Head>
+        <title dir={lang}>{postData.title}</title>
+      </Head>
+      <article >
+        <h1 dir={lang} className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dir={lang}  dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
